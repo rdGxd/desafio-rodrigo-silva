bcf40fa341fba0c944aeda1be32c3d7874543c5b
Rodrigo Silva <rdg.hora1@gmail.com>
2023-08-13 15:18:06 -0300

    Update caixa-da-lanchonete.js

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,5 +1,7 @@
+// Definição da classe CaixaDaLanchonete
 export class CaixaDaLanchonete {
   constructor() {
+    // Inicialização do cardápio com itens e seus preços correspondentes
     this.cardapio = {
       cafe: 3.0,
       chantily: 1.5,
@@ -12,35 +14,46 @@ export class CaixaDaLanchonete {
     };
   }
 
+  // Método para calcular o valor total da compra
   calcularValorDaCompra(formaDePagamento, itens) {
+    // Lista de formas de pagamento válidas
     const formasDePagamentoValidas = ["debito", "credito", "dinheiro"];
 
+    // Verificação da forma de pagamento válida
     if (!formasDePagamentoValidas.includes(formaDePagamento)) {
       return "Forma de pagamento inválida!";
     }
 
+    // Inicialização do carrinho de compras vazio e valor total
     const carrinho = {};
     let valorTotal = 0;
 
+    // Loop para processar cada item no pedido
     for (const itemInfo of itens) {
+      // Separação do código do item e sua quantidade
       const [codigo, quantidade] = itemInfo.split(",");
 
+      // Conversão da quantidade para número e verificação se é válida
       let checandoQuantidade = Number(quantidade);
       if (checandoQuantidade <= 0) return "Quantidade inválida!";
 
+      // Verificação se o item está no cardápio
       if (!this.cardapio[codigo]) {
         return "Item inválido!";
       }
 
+      // Verificação de condições para adicionar o item ao carrinho
       if (
         (this.cardapio[codigo] && codigo !== "chantily") ||
         (this.cardapio[codigo] && codigo !== "queijo")
       ) {
+        // Criação ou incremento da quantidade do item no carrinho
         if (!carrinho[codigo]) {
           carrinho[codigo] = 0;
         }
         carrinho[codigo] += Number(quantidade);
       }
+      // Verificação de itens extras sem o item principal correspondente
       if (codigo === "chantily") {
         if (!carrinho["cafe"]) {
           return "Item extra não pode ser pedido sem o principal";
@@ -53,23 +66,24 @@ export class CaixaDaLanchonete {
       }
     }
 
+    // Verificação se o carrinho está vazio
     if (Object.keys(carrinho).length === 0) {
       return "Não há itens no carrinho de compra!";
     }
 
+    // Cálculo do valor total da compra com base no carrinho
     for (const codigo in carrinho) {
       valorTotal += this.cardapio[codigo] * carrinho[codigo];
     }
 
+    // Aplicação de desconto ou acréscimo com base na forma de pagamento
     if (formaDePagamento === "dinheiro") {
       valorTotal *= 0.95;
     } else if (formaDePagamento === "credito") {
       valorTotal *= 1.03;
     }
 
+    // Retorno do valor total formatado como uma string
     return `R$ ${valorTotal.toFixed(2).replace(".", ",")}`;
   }
 }
-
-// Exemplos de uso:
-const caixa = new CaixaDaLanchonete();
