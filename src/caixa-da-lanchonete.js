e35b775c1505011ba480469b93c910cd647dd3c8
Rodrigo Silva <rdg.hora1@gmail.com>
2023-08-16 08:03:16 -0300

    Finalizado

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,65 +1,73 @@
+// Definição da classe CaixaDaLanchonete
 export class CaixaDaLanchonete {
   constructor() {
     // Inicializando o cardápio com os preços dos itens
     this.cardapio = {
-      cafe: 3.0,
-      chantily: 1.5,
-      suco: 6.2,
-      sanduiche: 6.5,
-      queijo: 2.0,
-      salgado: 7.25,
-      combo1: 9.5,
-      combo2: 7.5,
+      cafe: 3.0, // Preço do café
+      chantily: 1.5, // Preço do chantilly
+      suco: 6.2, // Preço do suco
+      sanduiche: 6.5, // Preço do sanduíche
+      queijo: 2.0, // Preço do queijo
+      salgado: 7.25, // Preço do salgado
+      combo1: 9.5, // Preço do combo 1
+      combo2: 7.5, // Preço do combo 2
     };
   }
 
+  // Método para calcular o valor da compra
   calcularValorDaCompra(formaPagamento, itens) {
-    const formasValidas = ["debito", "dinheiro", "credito"];
+    const PagamentoValidos = ["dinheiro", "credito", "debito"];
 
-    // Verificando se a forma de pagamento é válida
-    if (!formasValidas.includes(formaPagamento)) {
+    const carrinho = {}; // Carrinho de compras vazio
+    let valorTotal = 0; // Valor total da compra inicializado
+
+    // Verificação da forma de pagamento
+    if (!PagamentoValidos.includes(formaPagamento)) {
       return "Forma de pagamento inválida!";
     }
 
-    const carrinho = [];
-    let valorTotal = 0;
+    // Verificação de itens no carrinho
+    if (itens <= 0) return "Não há itens no carrinho de compra!";
 
+    // Iteração sobre os itens no carrinho
     for (const itemInfo of itens) {
-      const [codigo, quantidade] = itemInfo.split(",");
+      const [item, quantidade] = itemInfo.split(",");
 
-      // Checando quantidade do item
-      const checandoQuantidade = Number(quantidade);
-      if (checandoQuantidade <= 0) return "Quantidade inválida!";
+      // Verificação de quantidade inválida
+      if (Number(quantidade) <= 0) return "Quantidade inválida!";
 
-      // Verificando se o item existe no cardápio
-      if (!this.cardapio[codigo]) return "Item inválido!";
+      // Verificação de item inválido
+      if (!this.cardapio[item]) return "Item inválido!";
 
-      // Verificando se os itens extras estão sendo pedidos com o item principal
-      if (codigo === "chantily") {
-        if (!carrinho.includes("cafe")) {
+      // Atualização do carrinho com os itens e quantidades
+      if (this.cardapio[item]) {
+        if (!carrinho[item]) carrinho[item] = quantidade;
+      }
+
+      // Verificação de itens adicionais
+      if (item === "chantily") {
+        if (!carrinho.cafe) {
           return "Item extra não pode ser pedido sem o principal";
         }
       }
 
-      if (codigo === "queijo") {
-        if (!carrinho.includes("sanduiche")) {
+      if (item === "queijo") {
+        if (!carrinho.sanduiche) {
           return "Item extra não pode ser pedido sem o principal";
         }
       }
-
-      carrinho.push(codigo);
-
-      // Calculando o valor total do carrinho
-      valorTotal += this.cardapio[codigo] * checandoQuantidade;
     }
 
-    if (carrinho.length === 0) {
-      return "Não há itens no carrinho de compra!";
-    } else {
-      // Aplicando desconto ou acréscimo de acordo com a forma de pagamento
-      if (formaPagamento === "dinheiro") valorTotal -= (valorTotal * 5) / 100;
-      if (formaPagamento === "credito") valorTotal += (valorTotal * 3) / 100;
-      return `R$ ${valorTotal.toFixed(2).replace(".", ",")}`;
+    // Cálculo do valor total da compra
+    for (const pedido in carrinho) {
+      valorTotal += carrinho[pedido] * this.cardapio[pedido];
     }
+
+    // Cálculo de desconto ou acréscimo baseado na forma de pagamento
+    if (formaPagamento === "dinheiro") valorTotal -= (valorTotal * 5) / 100;
+    if (formaPagamento === "credito") valorTotal += (valorTotal * 3) / 100;
+
+    // Retorno do valor total formatado como moeda
+    return `R$ ${valorTotal.toFixed(2).replace(".", ",")}`;
   }
 }
